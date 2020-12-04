1525ce0df605eeb64d1fcfdde987b66a76633ab2
Shantanu Telang <75466882+Shantanu2004@users.noreply.github.com>
2020-12-04 11:19:42 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fc9cb8804e716001efc39f8",
+				"5fc9cb8804e716001efc39fa"
+			]
+		},
+		"5fc9cb8804e716001efc39f8": {
+			"id": "5fc9cb8804e716001efc39f8",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fc9cb8804e716001efc39fa": {
+			"id": "5fc9cb8804e716001efc39fa",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
